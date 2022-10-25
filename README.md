4e6b4da66a6c8377a2641ddefbefb9ffb60cccdf
Btriquetra <braddbitson0301@gmail.com>
2022-10-25 11:15:57 -0400

    Added readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# news-app
+
+This app will help you find what tickers the news is talking about.
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -14,6 +14,7 @@ function HomePage() {
             <div className="most-bullish">
                 <MostBullish />
             </div>
+            blah blag
             <div className="most-bearish">
                 <MostBearish />
             </div>
